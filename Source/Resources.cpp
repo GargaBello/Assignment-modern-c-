ae746526295bfe6ada19dda689ffd3846c3a840e
Sebastian_Saastamoinen <sebastian.saastamoinen.3769@student.uu.se>
2026-01-03 00:54:24 +0100

    Raii classes for resources, not implemented with rest of code yet

--- Source/Resources.cpp
+++ Source/Resources.cpp
@@ -1,5 +1,5 @@
 #include "Resources.h"
-#include <iostream>;
+
 
 /* Todo: Initialize in constructor, check if multiple ship textures are used
 * Check how the textures are unloaded and add to destructor
@@ -21,3 +21,62 @@ void Resources::Unload()
 	UnloadTexture(alienTexture);
 }
 */
+
+ShipTexture::ShipTexture()
+{
+	shipTextures.push_back(LoadTexture("./Assets/Ship1.png"));
+	shipTextures.push_back(LoadTexture("./Assets/Ship2.png"));
+	shipTextures.push_back(LoadTexture("./Assets/Ship3.png"));
+
+	int posInVec = 0;
+	for (auto& texture : shipTextures) {
+		++posInVec;
+		if (texture.id == 0) {
+			throw std::runtime_error("Failed to load texture: ShipTexture" + posInVec);
+		}
+	}
+}
+
+ShipTexture::~ShipTexture() {
+	for (auto& textures : shipTextures) {
+		UnloadTexture(textures);
+	}
+
+	shipTextures.clear();
+}
+
+BarrierTexture::BarrierTexture() :
+	barrierTexture(LoadTexture("./Assets/Barrier.png"))
+{
+	if (barrierTexture.id == 0) {
+		throw std::runtime_error("Failed to load texture: Barrier.png");
+	}
+}
+
+BarrierTexture::~BarrierTexture() {
+	UnloadTexture(barrierTexture);
+}
+
+AlienTexture::AlienTexture() :
+	alienTexture(LoadTexture("./Assets/Alien.png"))
+{
+	if (alienTexture.id == 0) {
+		throw std::runtime_error("Failed to load texture: Alien.png");
+	}
+}
+
+AlienTexture::~AlienTexture() {
+	UnloadTexture(alienTexture);
+}
+
+LaserTexture::LaserTexture() :
+	laserTexture(LoadTexture("./Assets/Laser.png"))
+{
+	if (laserTexture.id == 0) {
+		throw std::runtime_error("Failed to load texture: Laser.png");
+	}
+}
+
+LaserTexture::~LaserTexture() {
+	UnloadTexture(laserTexture);
+}
--- Source/Resources.h
+++ Source/Resources.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "raylib.h"
-#include "vector"
+#include <vector>
+#include <string_view>
+#include <iostream>
+#include <exception>
+#include <ranges>
 
 /* TODO: Check if used, make loading part of construction
 * make destructor, check if rule of five is needed, 
@@ -18,5 +22,74 @@ struct Resources
 	Texture2D alienTexture;
 	Texture2D barrierTexture;
 	Texture2D laserTexture;
+
+};
+
+class ShipTexture {
+private:
+	std::vector<Texture2D> shipTextures{};
+public:
+
+	ShipTexture();
+	~ShipTexture();
+
+	ShipTexture(const ShipTexture&) = delete;
+	ShipTexture& operator=(const ShipTexture&) = delete;
+
+	ShipTexture(const ShipTexture&&) = delete;
+	ShipTexture& operator=(const ShipTexture&&) = delete;
+};
+
+class AlienTexture {
+private:
+	Texture2D alienTexture{};
+
+public:
+
+	AlienTexture();
+
+	~AlienTexture();
+
+	AlienTexture(const AlienTexture&) = delete;
+	AlienTexture& operator=(const AlienTexture&) = delete;
+
+	AlienTexture(const AlienTexture&&) = delete;
+	AlienTexture& operator=(const AlienTexture&&) = delete;
+
+};
+
+class BarrierTexture {
+private:
+	Texture2D barrierTexture{};
+
+public:
+
+	BarrierTexture();
+
+	~BarrierTexture();
+
+	BarrierTexture(const BarrierTexture&) = delete;
+	BarrierTexture& operator=(const BarrierTexture&) = delete;
+
+	BarrierTexture(const BarrierTexture&&) = delete;
+	BarrierTexture& operator=(const BarrierTexture&&) = delete;
+
+};
+
+class LaserTexture {
+private:
+	Texture2D laserTexture{};
+
+public:
+
+	LaserTexture();
+
+	~LaserTexture();
+
+	LaserTexture(const LaserTexture&) = delete;
+	LaserTexture& operator=(const LaserTexture&) = delete;
+
+	LaserTexture(const LaserTexture&&) = delete;
+	LaserTexture& operator=(const LaserTexture&&) = delete;
 
 };
